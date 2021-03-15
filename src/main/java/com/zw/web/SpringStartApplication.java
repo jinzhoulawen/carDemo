f8d8feb9220f4f9ca370d5c100e4d676c9c05b58
jinzhoulawen <1046618378@qq.com>
2021-03-15 18:03:23 +0800

    test:1

--- src/main/java/com/zw/web/SpringStartApplication.java
+++ src/main/java/com/zw/web/SpringStartApplication.java
@@ -1,9 +1,14 @@
 package com.zw.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@MapperScan("com.zw.web.mapper")
 public class SpringStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringStartApplication.class,args);
--- src/main/java/com/zw/web/controller/testContrller.java
+++ src/main/java/com/zw/web/controller/testContrller.java
@@ -1,14 +1,22 @@
 package com.zw.web.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.zw.web.model.domian.User;
+import com.zw.web.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class testContrller {
+    @Autowired
+    private UserInfoService userInfoService;
    @RequestMapping("/hello")
     public String sayHello(){
-      return "sayhello";
+//       List<User> list= userInfoService.selectUser();
+//       list.forEach(System.out::println);
+
+       return "sayhello";
   }
 }
